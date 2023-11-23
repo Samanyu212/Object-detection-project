b04f42c077d07770723c08070539c88ea915965c
Samanyu212 <121870712+Samanyu212@users.noreply.github.com>
2023-11-23 18:45:46 +0530

    Add files via upload

---
+++ Desk.js
@@ -0,0 +1,35 @@
+img="";
+
+function preload(){
+img=loadImage("Desk.jpg")
+}
+
+function setup(){
+canvas=createCanvas(640,420)
+canvas.center();
+objectDetector=ml5.objectDetector("cocoSSD",modelLoaded)
+}
+
+function draw(){
+image(img,0,0,540,320);
+
+
+
+}
+
+function modelLoaded(){
+    console.log("model is loaded");
+    Status=true;
+    objectDetector.detect(img,getResults);
+}
+
+function getResults(error,results){
+    if(error){
+        console.log(error);
+
+        
+    }
+    else{
+        console.log(results);
+    }
+}
